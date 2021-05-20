dfd3c1a3f20e47e64ab7a23d7f014bbc9cf912ae
Aakash Chaudhari <aakash03chaudhari@gmail.com>
2021-05-20 17:35:00 +0530

    Project and Header Component

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React, { Component } from 'react'
 import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Dashboard />
       </div>
     )
